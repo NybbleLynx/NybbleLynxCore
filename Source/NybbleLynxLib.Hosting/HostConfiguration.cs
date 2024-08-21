60f36cb97b56d9487964e06dea5bfbf7a1aca9bf
Rob Husband <rob.a.husband@gmail.com>
2024-08-21 19:49:24 +0100

    Added HostConfiguration

---
+++ Source/NybbleLynxLib.Hosting/HostConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace NybbleLynxLib.Hosting
+{
+    public class HostConfiguration : IHostConfig
+    {
+        private readonly IContainer container;
+
+        public HostConfiguration(IContainer container)
+        {
+            this.container = container ?? throw new ArgumentNullException(nameof(container));
+        }
+
+        /// <inheritdoc />
+        public IHostConfig Register<TInterface, TConcrete>() 
+            where TConcrete : class, TInterface
+        {
+            container.AddService<TInterface, TConcrete>(ServiceScope.Transient);
+            return this;
+        }
+
+        /// <inheritdoc />
+        public IHostConfig RegisterSingleton<TInterface, TConcrete>() 
+            where TConcrete : class, TInterface
+        {
+            container.AddService<TInterface, TConcrete>(ServiceScope.Singleton);
+            return this;
+        }
+
+        /// <inheritdoc />
+        public IHostConfig RegisterFactory<TFactory>() 
+            where TFactory : class
+        {
+            container.AddFactoryService<TFactory>();
+            return this;
+        }
+
+        /// <inheritdoc />
+        public IHostConfig RegisterSpecific<TInterface, TConcrete, TTarget>(bool allowDerivedTypes = false)
+            where TConcrete : class, TInterface where TTarget : class
+        {
+            container.AddTargetedService<TInterface, TConcrete, TTarget>(allowDerivedTypes);
+            return this;
+        }
+
+        /// <inheritdoc />
+        public IHostConfig RegisterConstant<TConcrete>(TConcrete value)
+        {
+            container.AddInstance(value);
+            return this;
+        }
+
+        /// <inheritdoc />
+        public IHostConfig Named(string name)
+        {
+            container.AddHostName(name);
+            return this;
+        }
+
+        /// <inheritdoc />
+        public IHostConfig IdentifiedAs(Guid identifier)
+        {
+            container.AddHostIdentifier(identifier);
+            return this;
+        }
+    }
+}
---
+++ Source/NybbleLynxLib.Hosting/IHostConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NybbleLynxLib.Hosting
+{
+    public interface IHostConfig
+    {
+        IHostConfig Register<TInterface, TConcrete>() where TConcrete : class, TInterface;
+        IHostConfig RegisterSingleton<TInterface, TConcrete>() where TConcrete : class, TInterface;
+        IHostConfig RegisterFactory<TFactory>() where TFactory : class;
+        IHostConfig RegisterSpecific<TInterface, TConcrete, TTarget>(bool allowDerivedTypes = false) where TConcrete : class, TInterface where TTarget : class;
+        IHostConfig RegisterConstant<TConcrete>(TConcrete value);
+
+        IHostConfig Named(string name);
+        IHostConfig IdentifiedAs(Guid identifier);
+    }
+}
